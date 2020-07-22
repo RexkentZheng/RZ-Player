26b3861ffc30c2ed351e90501651ce4231e5c6c2
rexkentzheng <331749897@qq.com>
2020-07-22 21:16:10 +0800

    '基本能用状态'

--- components/details/index.jsx
+++ components/details/index.jsx
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import tools from './../../lib/site/tools';
 import { View, Text, TouchableOpacity, StatusBar, ScrollView, Image } from 'react-native';
 import styles from './../style';
-import Video from './video';
 import Loading from './../loading/index';
 import {observer, inject} from 'mobx-react';
+import VideoPlayer from 'react-native-rn-videoplayer';
 
 @inject('store')
 @observer
@@ -12,6 +12,7 @@ export default class Details extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      fullScreen: false,
       loading: false,
       details: {
         m3u8_urls: [],
@@ -31,6 +32,8 @@ export default class Details extends Component {
         loading: false,
         details: res,
         m3u8Url: this.getUrl(res.m3u8_urls[0])
+      }, () => {
+        this.player.reLoad()
       })
     })
   }
@@ -52,20 +55,52 @@ export default class Details extends Component {
   }
 
   updateM3U8 (m3u8Url) {
-    this.setState({ m3u8Url })
+    this.setState({ m3u8Url }, () => {
+      this.player.reLoad()
+    })
   }
+
+  updateScreenType (status) {
+    this.setState({
+      fullScreen: status === 'full'
+    })
+  }
+
+  renderAllSeenList = () => (
+    <View style={{ backgroundColor: "rgba(0,0,0,0.6)", position: "absolute", top: 0, bottom: 0, right: 0, }}>
+        <ScrollView>
+          {
+            this.state.details.m3u8_urls.map((item, index) => {
+              return (
+                <TouchableOpacity
+                  style={styles.detailWrapperItem}
+                  key={`episo-${index}`}
+                  onPress={() => this.updateM3U8(this.getUrl(item))}>
+                  <Text style={styles.detailWrapperItemText}>{this.getName(item)}</Text>
+                </TouchableOpacity>
+              )
+            })
+          }
+        </ScrollView>      
+      </View>
+    )
 
   render() {
     const { navigation, store } = this.props;
 
     return (
-      <View style={styles.detail}>
-        <StatusBar barStyle="dark-content" hidden={store.fullScreen} />
+      <ScrollView style={styles.detail}>
+        <StatusBar barStyle="dark-content" hidden={this.state.fullScreen} />
         <Loading loading={this.state.loading} text='正在载入详情，请稍候' />
-        <View style={styles.detailVideoWrapper}>
-          <Video videoUrl={this.state.m3u8Url} navigation={navigation}/>
-        </View>
-        <ScrollView style={{flex: 1, marginTop: -190, display: `${store.fullScreen ? 'none' : 'flex'}`}}>
+        <VideoPlayer
+            url={this.state.m3u8Url}
+            ref={(ref)=>this.player=ref}
+            navigation={navigation}//路由 用于小屏屏播放的返回按钮
+            onWindowChange={(status) => this.updateScreenType(status)}
+            continuous={true}
+            renderAllSeenList={this.renderAllSeenList}
+          />
+          <View style={{flex: 1, display: `${this.state.fullScreen ? 'none' : 'flex'}`}}>
             <Text style={styles.detailTitle}>{this.state.details.name}</Text>
             <View style={styles.detailImgWrapper}>
               <Image
@@ -87,8 +122,8 @@ export default class Details extends Component {
                 })
               }
             </View>
-        </ScrollView>
-      </View>
+        </View>
+      </ScrollView>
     )
   }
 }
